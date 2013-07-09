65e874054581ec29932dc007b958be1d0d325cb6
Gazok <jamiebayne@hotmail.com>
2013-07-09 21:23:12 +0100

    Added Resources file and test resource

---
+++ src/Resources.hpp
@@ -0,0 +1,10 @@
+#ifndef RESOURCES_HPP
+#define RESOURCES_HPP
+
+#include "Tank/Graphics/Image.hpp"
+
+namespace res
+{
+    const tank::Image menuStart("res/gfx/play.png");
+}
+#endif /* RESOURCES_HPP */
