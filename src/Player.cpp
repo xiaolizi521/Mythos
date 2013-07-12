d174c5c1ef2af4ed846be1268b81c770c3997b56
Gazok <jamiebayne@hotmail.com>
2013-07-12 16:57:47 +0100

    Added player movement

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,11 +1,15 @@
 #include "Player.hpp"
 
+#include <cmath>
 #include "Resources.hpp"
+
+const float Player::speed_ = 1.f;
+
 Player::Player(tank::Vectorf pos, Level& l, GameState& gs)
     : GameboyEntity(pos)
-    , moving_(false)
     , level_(l)
     , gameState_(gs)
+    , moving_(false)
 {
     anim_ = makeGraphic<tank::Animation>(res::player,
                                          tank::Vectoru{ level_.getTileSize(),
@@ -19,39 +23,70 @@ Player::Player(tank::Vectorf pos, Level& l, GameState& gs)
     auto& evtHandler = gameState_.getEventHandler();
     evtHandler.registerEvent("playerMove", tank::EventType::button_held);
     evtHandler.registerEventListener("playerMove",
-                                     std::bind(&Player::move, this,
+                                     std::bind(&Player::handleInput, this,
                                                std::placeholders::_1));
     evtHandler.registerKey(sf::Keyboard::W, "playerMove");
     evtHandler.registerKey(sf::Keyboard::D, "playerMove");
     evtHandler.registerKey(sf::Keyboard::S, "playerMove");
     evtHandler.registerKey(sf::Keyboard::A, "playerMove");
+
+    level_.setCamera(getPos() + 8);
 }
 
-bool Player::move(tank::EventArgs const& args)
+void Player::update()
+{
+    if (moving_)
+    {
+        tank::Vectorf pos = getPos();
+
+        if (std::abs(toMove_) < speed_)
+        {
+            pos += direction_ * toMove_;
+            toMove_ = 0;
+            moving_ = false;
+        }
+        else
+        {
+            pos += direction_ * speed_;
+            toMove_ -= speed_;
+        }
+
+        setPos(pos);
+        level_.setCamera(pos + 8);
+    }
+}
+bool Player::handleInput(tank::EventArgs const& args)
 {
     if(moving_)
     {
         return true;
     }
 
+    direction_ = {};
+    toMove_ = level_.getTileSize();
+
     switch (args.key)
     {
     case sf::Keyboard::W:
         anim_->select("up");
+        direction_.y = - 1;
         break;
     case sf::Keyboard::D:
         anim_->select("right");
+        direction_.x = 1;
         break;
     case sf::Keyboard::S:
         anim_->select("down");
+        direction_.y = 1;
         break;
     case sf::Keyboard::A:
         anim_->select("left");
+        direction_.x = -1;
         break;
     default:
         break;
     }
 
-    //moving_ = true;
+    moving_ = true;
     return true;
 }
--- src/Player.hpp
+++ src/Player.hpp
@@ -12,13 +12,19 @@ class Player : public GameboyEntity
 {
 public:
     Player(tank::Vectorf pos, Level& level, GameState& gameState);
+
+    virtual void update() override;
 private:
     tank::Animation* anim_;
-    bool moving_;
     Level& level_;
     GameState& gameState_;
 
-    bool move(tank::EventArgs const& e);
+    bool moving_;
+    tank::Vectorf direction_;
+    float toMove_;
+
+    static const float speed_;
+    bool handleInput(tank::EventArgs const& e);
 };
 
 #endif /* PLAYER_HPP */
